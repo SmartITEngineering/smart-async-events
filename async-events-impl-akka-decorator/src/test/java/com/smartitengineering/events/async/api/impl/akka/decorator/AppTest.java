a273a806d36922dcabc1b24173a2f84d76a7c5f8
Imran M Yousuf <imyousuf@smartitengineering.com>
2011-08-01 16:05:53 +0600

    Add simple test case to test fault tolerant typed actor
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- async-events-impl-akka-decorator/src/test/java/com/smartitengineering/events/async/api/impl/akka/decorator/AppTest.java
+++ async-events-impl-akka-decorator/src/test/java/com/smartitengineering/events/async/api/impl/akka/decorator/AppTest.java
@@ -1,38 +1,45 @@
 package com.smartitengineering.events.async.api.impl.akka.decorator;
 
-import junit.framework.Test;
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+import com.smartitengineering.events.async.api.EventPublisher;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.Sequence;
+import org.jmock.integration.junit3.JUnit3Mockery;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  private static final Mockery mockery = new JUnit3Mockery();
+  private static final EventPublisher mock = mockery.mock(EventPublisher.class);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+  public void testTypedActorBasedImplementationWithFailures() {
+    mockery.checking(new Expectations() {
+
+      {
+        Sequence sequence = mockery.sequence("name");
+        exactly(3).of(mock).publishEvent("a", "a");
+        will(throwException(new EventPublicationException(new NullPointerException())));
+        inSequence(sequence);
+        exactly(1).of(mock).publishEvent("a", "a");
+        will(returnValue(true));
+        inSequence(sequence);
+      }
+    });
+    EventPublisher publisher = new EventPublisherImpl();
+    publisher.publishEvent("a", "a");
+  }
+
+  public static class Module extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+      bind(EventPublisher.class).annotatedWith(Names.named("decorateePublisher")).toInstance(mock);
     }
+  }
 }
